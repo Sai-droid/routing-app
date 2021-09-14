3670f74e5deb25133980f582354daf0f06907cb7
Saicharan <kalakonda.saicharan@capgemini.com>
2021-09-14 23:48:36 +0530

    routing all the react components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import { BrowserRouter, Route, NavLink ,Prompt,Redirect} from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import "./App.css";
 
 function App() {
+  const[loggin,setLoggin] = useState(false)
+  const[age,setAge] = useState(null)
+
+  const logInHandle =()=>{
+    setLoggin(!loggin)
+  }
+  
+  const ageHandle =(event)=>{
+    setAge(event.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <ul className ="ul-style ">
+            <li className ="li-style">
+              <NavLink className ="App-Link" exact  to="/" activeClassName="active-link-style"> Home</NavLink>
+            </li>
+            <li className ="li-style" >
+              <NavLink className ="App-Link" exact to="/about" activeClassName="active-link-style" > About </NavLink>
+            </li>
+            <li className ="li-style" >
+              <NavLink className ="App-Link" exact to="/user/john/doe" activeClassName="active-link-style" > user john doe </NavLink>
+            </li>
+          </ul>
+          <Prompt when ={loggin && !age} message ={(location)=>{
+            return location.pathname.startsWith('/user')?true:"are you sure"
+          }} />
+            
+          {loggin.toString()}
+            <button className="button-style" onClick={logInHandle}>{loggin?"logout":"loggin"}</button>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return <h1>Welcome Home</h1>;
+            }}
+          />
+          <Route exact path="/about" component={AboutPage} />
+          <Route
+            exact
+            path="/user/:username/:lastname"
+            render={({ match }) => {
+              return loggin ? (
+                <>
+                <h1>age:{age}</h1>
+                <input type ="text" value={age} onChange={ageHandle}></input>
+                <h1>
+                  Welcome {match.params.username} {match.params.lastname}
+                </h1>
+                </>
+              ):<Redirect to="/" />;
+            }}
+          />
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutPage() {
+    return (
+        <h1>
+            welcome to About Page
+        </h1>
+    )
+}
+
+export default AboutPage
